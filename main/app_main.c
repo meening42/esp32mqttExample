4a575af39b35f76f38c6b7fbb798484d5c67ea6f
marko <marko.antoncic@gmail.com>
2024-07-31 22:54:00 +0200

    timer and button working

--- main/app_main.c
+++ main/app_main.c
@@ -15,6 +15,31 @@
 #include "protocol_examples_common.h"
 #include "esp_log.h"
 #include "mqtt_client.h"
+#include <inttypes.h>
+#include "driver/gpio.h"
+#include "driver/temperature_sensor.h"
+
+#define ESP_INTR_FLAG_DEFAULT 0
+#define GPIO_INPUT_IO_0     9
+#define GPIO_INPUT_PIN_SEL  (1ULL<<GPIO_INPUT_IO_0) 
+
+temperature_sensor_handle_t temp_sensor = NULL;
+
+
+typedef enum  {
+  TRIGGER_BTN,
+  TRIGGER_TIMER,
+  TRIGGER_MQTT_REQUEST
+}triggerSrc_t;
+
+static QueueHandle_t evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    triggerSrc_t trigger = TRIGGER_BTN;
+    xQueueSendFromISR(evt_queue, &trigger, NULL);
+}
+
 
 static const char *TAG = "mqtt5_example";
 
@@ -95,9 +120,8 @@ int id = 1;
 int interval = 5000;
 void ping( TimerHandle_t xTimer )
 {
-    printf("Timer!\n");
-
- // ESP_LOGI(TAG,"tring tring!!!");
+    triggerSrc_t trigger = TRIGGER_TIMER;
+    xQueueSendFromISR(evt_queue, &trigger, NULL);
 }
 
 
@@ -169,21 +193,15 @@ static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32
         esp_mqtt5_client_set_disconnect_property(client, &disconnect_property);
         esp_mqtt5_client_delete_user_property(disconnect_property.user_property);
         disconnect_property.user_property = NULL;
-        esp_mqtt_client_disconnect(client);
+        //esp_mqtt_client_disconnect(client);
         break;
     case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        print_user_property(event->property->user_property);
+        //ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        //print_user_property(event->property->user_property);
         break;
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         print_user_property(event->property->user_property);
-        ESP_LOGI(TAG, "payload_format_indicator is %d", event->property->payload_format_indicator);
-        ESP_LOGI(TAG, "response_topic is %.*s", event->property->response_topic_len, event->property->response_topic);
-        ESP_LOGI(TAG, "correlation_data is %.*s", event->property->correlation_data_len, event->property->correlation_data);
-        ESP_LOGI(TAG, "content_type is %.*s", event->property->content_type_len, event->property->content_type);
-        ESP_LOGI(TAG, "TOPIC=%.*s", event->topic_len, event->topic);
-        ESP_LOGI(TAG, "DATA=%.*s", event->data_len, event->data);
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
@@ -274,29 +292,82 @@ static void mqtt5_app_start(void)
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt5_event_handler, NULL);
     esp_mqtt_client_start(client);
 
-    int msg_id;
-    //print_user_property(event->property->user_property);
-    esp_mqtt5_client_set_user_property(&publish_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
-    esp_mqtt5_client_set_publish_property(client, &publish_property);
-    msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "hej", 0, 1, 1);
-    esp_mqtt5_client_delete_user_property(publish_property.user_property);
-    publish_property.user_property = NULL;
+    esp_mqtt_client_subscribe(client, "tempRequest", 0);
+}
+
+
+
+void  initBtnGpio(){
+   //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio interrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_POSEDGE);
+}
+
+static void myTask(void* arg)
+{
+    uint32_t prevTick=0;
+    triggerSrc_t src;
+    float tsens_value;
+    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(10, 50);
+    ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor_config, &temp_sensor));
+    ESP_LOGI(TAG, "Enable temperature sensor");
+    ESP_ERROR_CHECK(temperature_sensor_enable(temp_sensor));
+    char buffer[64];
+    snprintf(buffer, sizeof(buffer), "My task started\n");
+    printf("%s",buffer);
+    for (;;) {
+        if (xQueueReceive(evt_queue, &src, portMAX_DELAY)) {
+            temperature_sensor_get_celsius(temp_sensor, &tsens_value);
+            switch(src){
+                case TRIGGER_BTN:                   
+                    snprintf(buffer, sizeof(buffer), "btn pressed, temp = %.1f\n", tsens_value);
+                    break;
+                case TRIGGER_TIMER:
+                    snprintf(buffer, sizeof(buffer), "timer 5s, temp = %.1f\n", tsens_value);
+                    break;
+                case TRIGGER_MQTT_REQUEST:   
+                    snprintf(buffer, sizeof(buffer), "mqtt req., temp = %.1f\n", tsens_value);
+                    break;
+                default:
+                    snprintf(buffer, sizeof(buffer), "unknown trigger, temp = %.1f\n", tsens_value);
+                    break;
+            }
+            printf("%s",buffer);
+            esp_mqtt_client_publish(client, "temperature", buffer, 0, 1, 1);
+        }
+    }
 }
 
 void app_main(void)
 {
+ 
+    // initialize Btn input
+    initBtnGpio();
+
+    //create a queue to handle gpio event from isr
+    evt_queue = xQueueCreate(10, sizeof(triggerSrc_t));
+    //start task
+    xTaskCreate(myTask, "myTask", 2048, NULL, 10, NULL);
+
+        //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
 
     ESP_LOGI(TAG, "[APP] Startup..");
     ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
-    esp_log_level_set("mqtt_example", ESP_LOG_VERBOSE);
-    esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
-    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
-    esp_log_level_set("transport", ESP_LOG_VERBOSE);
-    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
@@ -308,8 +379,6 @@ void app_main(void)
      */
     ESP_ERROR_CHECK(example_connect());
 
-
-
     mqtt5_app_start();
 
     tmr = xTimerCreate("MyTimer", pdMS_TO_TICKS(interval), pdTRUE, ( void * )id, &ping);
@@ -317,3 +386,6 @@ void app_main(void)
      printf("Timer start error");
     }
 }
+
+
+
